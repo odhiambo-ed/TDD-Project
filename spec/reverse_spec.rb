a9e570e5b88edf598b24b179540ad88d598d5f31
sainingo <nickmentor421@gmail.com>
2022-07-01 16:10:11 +0300

    Fix linter error

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -1,12 +1,11 @@
 require './solver'
-​
 describe Solver do
   context 'Should call reverse method and return the reverse of string' do
     it 'Ahmed should return demhA' do
       solver = Solver.new
       expect(solver.reverse('Ahmed')).to eq 'demhA'
     end
-    ​
+
     it 'Chimwemwe should return ewmewmeihC' do
       solver = Solver.new
       expect(solver.reverse('Chimwemwe')).to eq 'ewmewmihC'
